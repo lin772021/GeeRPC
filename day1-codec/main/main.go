305b77f713b142c69200d4809d688e1bbd765365
lin772021 <linlin188043352483@gmail.com>
2025-04-08 20:57:22 +0800

    day 1 geerpc

---
+++ day1-codec/main/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"geerpc"
+	"geerpc/codec"
+	"log"
+	"net"
+	"time"
+)
+
+func startServer(addr chan string) {
+	// pick a free port
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	geerpc.Accept(l)
+}
+
+func main() {
+	addr := make(chan string)
+	go startServer(addr)
+
+	// in fact, following code is like a simple geerpc client
+	conn, _ := net.Dial("tcp", <-addr)
+	defer func() { _ = conn.Close() }()
+
+	time.Sleep(time.Second)
+	// send options
+	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
+	cc := codec.NewGobCodec(conn)
+	// send request & receive response
+	for i := 0; i < 5; i++ {
+		h := &codec.Header{
+			ServiceMethod: "Foo.Sum",
+			Seq:           uint64(i),
+		}
+		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq)) //send the request
+		_ = cc.ReadHeader(h)                                 //read the response header from server
+		var reply string
+		_ = cc.ReadBody(&reply) // read the response body from server
+		log.Println("reply:", reply)
+	}
+}
