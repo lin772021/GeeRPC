dcf4a83b5c9cb35fd69a9c892f0494f318a2a9bc
lin772021 <linlin188043352483@gmail.com>
2025-04-09 16:57:54 +0800

    day 2

--- day1-codec/main/main.go
+++ day1-codec/main/main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"geerpc"
-	"geerpc/codec"
 	"log"
 	"net"
+	"sync"
 	"time"
 )
 
@@ -26,23 +25,39 @@ func main() {
 	go startServer(addr)
 
 	// in fact, following code is like a simple geerpc client
-	conn, _ := net.Dial("tcp", <-addr)
-	defer func() { _ = conn.Close() }()
+	client, _ := geerpc.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
 
 	time.Sleep(time.Second)
-	// send options
-	_ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
-	cc := codec.NewGobCodec(conn)
-	// send request & receive response
+	// // send options
+	// _ = json.NewEncoder(conn).Encode(geerpc.DefaultOption)
+	// cc := codec.NewGobCodec(conn)
+	// // send request & receive response
+	// for i := 0; i < 5; i++ {
+	// 	h := &codec.Header{
+	// 		ServiceMethod: "Foo.Sum",
+	// 		Seq:           uint64(i),
+	// 	}
+	// 	_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq)) //send the request
+	// 	_ = cc.ReadHeader(h)                                 //read the response header from server
+	// 	var reply string
+	// 	_ = cc.ReadBody(&reply) // read the response body from server
+	// 	log.Println("reply:", reply)
+	// }
+
+	// send requst & receive response
+	var wg sync.WaitGroup
 	for i := 0; i < 5; i++ {
-		h := &codec.Header{
-			ServiceMethod: "Foo.Sum",
-			Seq:           uint64(i),
-		}
-		_ = cc.Write(h, fmt.Sprintf("geerpc req %d", h.Seq)) //send the request
-		_ = cc.ReadHeader(h)                                 //read the response header from server
-		var reply string
-		_ = cc.ReadBody(&reply) // read the response body from server
-		log.Println("reply:", reply)
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
 	}
+	wg.Wait()
 }
